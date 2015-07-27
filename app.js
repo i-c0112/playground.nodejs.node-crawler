b25cfd3b3028d88f38fbf25ff8345bb32d83ebea
i-c0112 <a7807118@gmail.com>
2015-07-27 10:47:18 +0800

    -- Try to parse item page.

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ var Crawler = require("crawler").Crawler;
 var shell = require("shelljs");
 var cheerio = require("cheerio");
 
+var extractDesc = function (error, result, $) {
+	if (error) {
+		shell.echo(error);
+		return;
+	}
+	$ = cheerio.load(result.body);
+	var fname = $("span[itemprop=productID]").text();
+	shell.echo($(".desc").html()).to("output/" + fname + ".html");
+};
+
 var c = new Crawler({
 	"maxConnections": 10,
 	"userAgent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
+	"jQuery": false,
 	"callback": function(error, result, $) {
 		// bugfix: crawler somehow fail to load cheerio into $
 		$ = cheerio.load(result.body);
@@ -15,12 +26,14 @@ var c = new Crawler({
 		}
 
 		$("#rank > div").eq(2).children().eq(1).find("li > a").each(function (index, a) {
-			shell.echo($(a).attr("href"));
+			c.queue({
+				uri: $(a).attr("href"),
+				callback: extractDesc,
+			});
 		});
 	},
 });
 
 c.queue([{
 	uri: "https://tw.mall.yahoo.com/152982958-category.html",
-	jQuery: false,
 }]);
